5c8d3fa867c0925b4015575d768e0b8a2e84f622
Julian Brady <julian.a.brady@icloud.com>
2026-06-13 14:31:31 -0400

    Fixing typos
    Removed duplicate global variable declarations and fixed pin definition for MOSFET.

--- BR-80_Turret.ino
+++ BR-80_Turret.ino
@@ -3,16 +3,18 @@
 //Libraries
 #include <Stepper.h>
 
+//Global Variables
+const int stepsPerRevolution = 2048; // Defines the number of steps per rotation
+bool calAct = 1; //setting calibration requirent on at the start of the code
+int calState; //stores value of trigger motor
+int fireState; //stores value of trigger button
+
 //Pin definitions
 #define Trig_cal A0 //microswitch to calibrate trigger motor position
 #define Joy_button A1 //Joystick button press - triggers firing sequence
 #define MOSFET 2 //MOSFET triggers flywheel motors
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11); // Creates an instance of stepper class
   // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-
-//Global Variables
-const int stepsPerRevolution = 2048; // Defines the number of steps per rotation
-bool calAct = 1; //setting calibration requirent on at the start of the code
 
 
 void setup() {
@@ -34,20 +36,20 @@ void loop() {
   if(calAct == 1){
     //Stepper motor will move CCW by one step at a time until the microswitch is pressed
     do{
-    int calState = digitalRead(Trig_cal); //reading the microswitch
+    calState = digitalRead(Trig_cal); //reading the microswitch
     myStepper.step(-1); //1 step CCW
     }while(calState != LOW); 
-    
+
     calAct = 0; //removing calibration requirement
   }
 
   //reading the joystick's button to determine whether firing sequence should be entered
-  int fireState = digitalRead(Joy_button); 
+  fireState = digitalRead(Joy_button); 
 
   if(fireState == LOW) {
     //--~~ Firing Sequence ~~--
     digitalWrite(LED_BUILTIN, HIGH); // LED on if button is pressed
-    digitalWrite(MOSFET_PIN, HIGH);
+    digitalWrite(MOSFET, HIGH);
     delay(1500); //letting flywheels rev up
 
     // Rotate CW 180 degrees (pushing dart out of barrel)
@@ -55,10 +57,10 @@ void loop() {
     delay(100);
 
     calAct = 1; //requires code to re calibrate at the start of the loop
-    digitalWrite(MOSFET_PIN, LOW);
+    digitalWrite(MOSFET, LOW);
 
   } else {
     digitalWrite(LED_BUILTIN, LOW);  // LED is off unless button is pressed
-    digitalWrite(MOSFET_PIN, LOW);
+    digitalWrite(MOSFET, LOW);
   }
 }
